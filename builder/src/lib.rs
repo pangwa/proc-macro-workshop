b7b31cb0eb803623a69c03524ef565ba9b52c81d
pangwa <pangwa@gmail.com>
2022-08-11 16:37:57 +0800

    test 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -13,9 +13,30 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = &ast.ident;
     let builder_name = format!("{}BuilderImpl", name);
     let builder_type = syn::Ident::new(&builder_name, Span::call_site());
+    let mut fields_vec_innards = quote!();
+    match ast.fields {
+        syn::Fields::Named(named) => {
+            for field in named.named.iter() {
+                let fname = field.ident.as_ref().unwrap();
+                let ty = &field.ty;
+                fields_vec_innards.extend(quote! {
+                    pub fn #fname(&mut self, v: #ty) {
+
+                    }
+                });
+            }
+        }
+        _ => unimplemented!(),
+    }
 
     let gen = quote! {
-        pub struct #builder_type{}
+        pub struct #builder_type{
+        }
+
+        impl #builder_type {
+            #fields_vec_innards
+        }
+
         impl #name {
             fn builder() -> #builder_type{
                 #builder_type{}
