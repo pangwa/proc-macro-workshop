4b6b76c812dd714f34c9f6623b999f5ca6d6e0ab
pangwa <pangwa@gmail.com>
2022-08-11 16:55:45 +0800

    test 04, 05

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -13,16 +13,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = &ast.ident;
     let builder_name = format!("{}BuilderImpl", name);
     let builder_type = syn::Ident::new(&builder_name, Span::call_site());
-    let mut fields_vec_innards = quote!();
+    let mut init_values = quote!();
+    let mut fields_vec = quote!();
+    let mut setter_vec = quote!();
+    let mut builder_vec = quote!();
+    let mut result_vec = quote!();
     match ast.fields {
         syn::Fields::Named(named) => {
             for field in named.named.iter() {
                 let fname = field.ident.as_ref().unwrap();
                 let ty = &field.ty;
-                fields_vec_innards.extend(quote! {
-                    pub fn #fname(&mut self, v: #ty) {
-
+                init_values.extend(quote! {
+                    #fname: None,
+                });
+                fields_vec.extend(quote! {
+                    #fname: Option<#ty>,
+                });
+                setter_vec.extend(quote! {
+                    fn #fname(&mut self, v: #ty) -> &mut Self {
+                        self.#fname = Some(v);
+                        self
                     }
+                });
+                builder_vec.extend(quote! {
+                    if self.#fname == None {
+                        return None;
+                    }
+                });
+                result_vec.extend(quote! {
+                    #fname: self.#fname.clone().unwrap(),
                 });
             }
         }
@@ -31,15 +50,25 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let gen = quote! {
         pub struct #builder_type{
+            #fields_vec
         }
 
         impl #builder_type {
-            #fields_vec_innards
+            #setter_vec
+
+            fn build(&self) -> Option<#name> {
+                #builder_vec
+                Some(#name { #result_vec })
+            }
+
+            fn new() -> Self {
+                Self { #init_values }
+            }
         }
 
         impl #name {
             fn builder() -> #builder_type{
-                #builder_type{}
+                #builder_type::new()
             }
         }
 
