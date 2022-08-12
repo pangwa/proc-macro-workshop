0d4d214e7c3edff662eeb7e49bb2253e807dce23
pangwa <pangwa@gmail.com>
2022-08-12 18:33:53 +0800

    test 07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,52 @@
 use proc_macro::TokenStream;
-extern crate proc_macro;
 extern crate proc_macro2;
 extern crate syn;
 use proc_macro2::Span;
 
 #[macro_use]
 extern crate quote;
+
+fn extract_type_from_vec(ty: &syn::Type) -> Option<&syn::Type> {
+    use syn::{GenericArgument, Path, PathArguments, PathSegment};
+
+    fn extract_type_path(ty: &syn::Type) -> Option<&Path> {
+        match *ty {
+            syn::Type::Path(ref typepath) if typepath.qself.is_none() => Some(&typepath.path),
+            _ => None,
+        }
+    }
+
+    fn extract_vec_segment(path: &Path) -> Option<&PathSegment> {
+        let idents_of_path = path
+            .segments
+            .iter()
+            .into_iter()
+            .fold(String::new(), |mut acc, v| {
+                acc.push_str(&v.ident.to_string());
+                acc.push('|');
+                acc
+            });
+        vec!["Vec|"]
+            .into_iter()
+            .find(|s| &idents_of_path == *s)
+            .and_then(|_| path.segments.last())
+    }
+
+    extract_type_path(ty)
+        .and_then(|path| extract_vec_segment(path))
+        .and_then(|path_seg| {
+            let type_params = &path_seg.arguments;
+            // It should have only on angle-bracketed param ("<String>"):
+            match *type_params {
+                PathArguments::AngleBracketed(ref params) => params.args.first(),
+                _ => None,
+            }
+        })
+        .and_then(|generic_arg| match *generic_arg {
+            GenericArgument::Type(ref ty) => Some(ty),
+            _ => None,
+        })
+}
 
 fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
     use syn::{GenericArgument, Path, PathArguments, PathSegment};
@@ -49,7 +90,7 @@ fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
         })
 }
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast: syn::ItemStruct = syn::parse(input).unwrap();
     let name = &ast.ident;
@@ -58,32 +99,41 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut init_values = quote!();
     let mut fields_vec = quote!();
     let mut setter_vec = quote!();
+    let mut each_setter_vec = quote!();
     let mut builder_vec = quote!();
     let mut result_vec = quote!();
+
     match ast.fields {
         syn::Fields::Named(named) => {
             for field in named.named.iter() {
                 let fname = field.ident.as_ref().unwrap();
                 let ty = &field.ty;
                 let maybe_option_ty = extract_type_from_option(ty);
-                let (filed_ty, setter_ty) = match maybe_option_ty {
-                    Some(_) => (quote!(#ty), quote!(#maybe_option_ty)),
-                    _ => (quote!(Option<#ty>), quote!(#ty)),
-                };
-                init_values.extend(quote! {
-                    #fname: None,
-                });
-                fields_vec.extend(quote! {
-                    #fname: #filed_ty,
-                });
-                setter_vec.extend(quote! {
-                    fn #fname(&mut self, v: #setter_ty) -> &mut Self {
-                        self.#fname = Some(v);
-                        self
+                let maybe_vec_ty = extract_type_from_vec(ty);
+                let (filed_ty, setter_ty) = match (maybe_option_ty, maybe_vec_ty) {
+                    (Some(_), _) => {
+                        init_values.extend(quote! {
+                            #fname: None,
+                        });
+                        result_vec.extend(quote! {
+                            #fname: self.#fname.clone(),
+                        });
+                        (quote!(#ty), quote!(#maybe_option_ty))
                     }
-                });
-                match maybe_option_ty {
-                    None => {
+                    (None, Some(_)) => {
+                        init_values.extend(quote! {
+                            #fname: Vec::new(),
+                        });
+
+                        result_vec.extend(quote! {
+                            #fname: self.#fname.clone(),
+                        });
+                        (quote!(#ty), quote!(#maybe_vec_ty))
+                    }
+                    _ => {
+                        init_values.extend(quote! {
+                            #fname: None,
+                        });
                         builder_vec.extend(quote! {
                             if self.#fname == None {
                                 return Err("field missing".into());
@@ -93,10 +143,55 @@ pub fn derive(input: TokenStream) -> TokenStream {
                         result_vec.extend(quote! {
                             #fname: self.#fname.clone().unwrap(),
                         });
+
+                        (quote!(Option<#ty>), quote!(#ty))
                     }
-                    Some(_) => result_vec.extend(quote! {
-                        #fname: self.#fname.clone(),
-                    }),
+                };
+
+                let each_attr = field
+                    .attrs
+                    .iter()
+                    .map(|attr| {
+                        if !attr.path.is_ident("builder") {
+                            return None;
+                        }
+                        let meta: syn::Meta =
+                            attr.parse_args().expect("builder requires each field");
+                        match meta {
+                            syn::Meta::NameValue(m) => match m.lit {
+                                syn::Lit::Str(s) => {
+                                    Some(syn::Ident::new(&s.value(), Span::call_site()))
+                                }
+                                _ => None,
+                            },
+                            _ => None,
+                        }
+                    })
+                    .find(|v| v.is_some())
+                    .flatten();
+
+                fields_vec.extend(quote! {
+                    #fname: #filed_ty,
+                });
+                if each_attr.is_none() {
+                    setter_vec.extend(quote! {
+                        fn #fname(&mut self, v: #setter_ty) -> &mut Self {
+                            self.#fname = Some(v);
+                            self
+                        }
+                    });
+                }
+
+                match each_attr {
+                    None => (),
+                    Some(name) => {
+                        each_setter_vec.extend(quote! {
+                        fn #name(&mut self, v: #maybe_vec_ty) -> &mut Self {
+                                  self.#fname.push(v);
+                                  self
+                             }
+                        });
+                    }
                 }
             }
         }
@@ -111,6 +206,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         impl #builder_type {
+            #each_setter_vec
+
             #setter_vec
 
             pub fn build(&self) -> Result<#name, Box<dyn Error>> {
--- builder/tests/07-repeated-field.rs
+++ builder/tests/07-repeated-field.rs
@@ -46,6 +46,7 @@ fn main() {
         .executable("cargo".to_owned())
         .arg("build".to_owned())
         .arg("--release".to_owned())
+        .current_dir("".to_owned())
         .build()
         .unwrap();
 
